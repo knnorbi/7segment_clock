4659744f8a337589cd5d9b456c3de9773a3a68f5
knnorbi <kluka.norbert@gmail.com>
2017-02-28 20:52:43 +0100

    kaki.

--- clock.c
+++ clock.c
@@ -1,3 +1,17 @@
-void main() {
+void init() {
+    ANSEL = 0;
+    ANSELH = 0;
+    C1ON_bit = 0;                      // Disable comparators
+    C2ON_bit = 0;
+    TRISA = 255;
+    TRISB = 0;
+    TRISD = 0;
+    TRISE = 0b1110;
+    PORTB = 0;
+    PORTD = 0;
+    I2C1_Init(100000);
+}
 
+void main() {
+    init();
 }
